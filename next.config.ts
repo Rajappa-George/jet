c6769112baeee1ceb203281b4c264c14621481a0
Rajappa-George <rajappageorge98@gmail.com>
2026-09-08 19:15:29 +0530

    Fix GitHub Pages route folders

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/jet",
-  assetPrefix: "/jet/",
+  assetPrefix: "/jet",
+  trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
