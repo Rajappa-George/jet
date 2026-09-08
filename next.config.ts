fb5753e4a3dcd94304682e1e6b97d7b9e1891bdd
Rajappa-George <rajappageorge98@gmail.com>
2026-09-08 18:44:26 +0530

    Prepare GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/jet-skills",
+  assetPrefix: "/jet-skills/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
